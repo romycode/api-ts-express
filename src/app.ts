71ec207289c7cec6df2e766ff7c70ba880797077
Albert Romera Mulero <albert.romera.arm@gmail.com>
2021-07-24 13:06:58 +0200

    feat: add ts-node-env for development environment

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,17 @@ import express, { Express } from "express"
 import helmet from "helmet"
 // Controller
 import { healthController } from "@/system/health.controller"
+import { welcomeController } from "@/system/welcome.controller"
 
 const app: Express = express()
 
-app.set("port", process.env["PORT"] || 3000)
+app.set("port", process.env["PORT"] || 8080)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(helmet())
 app.use(compression())
 
+app.get("/", welcomeController)
 app.get("/health", healthController)
 
 export { app }
---
+++ src/system/welcome.controller.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express"
+import { OK } from "http-codes"
+
+export const welcomeController = (_req: Request, res: Response): void => {
+    res.status(OK).json({data: "Welcome to ExpressJS API"})
+}
